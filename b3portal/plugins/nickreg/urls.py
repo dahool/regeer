876022cc0e66c4fdb5b20a8d289c37ef02f927c9
dahool <gabriel@opensuse.org>
2011-07-15 12:38:52 -0300

    remove unused plugins

--- b3portal/plugins/nickreg/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import *
-import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.home, name='follow_home'),
-    url(r'^add/(?P<id>[0-9]+)/$', views.add, name='follow_add'),
-    url(r'^del/(?P<id>[0-9]+)/$', views.remove, name='follow_remove'),
-)
--- b3portal/plugins/stats/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from b3connect.admin import CustomModelAdmin
-from models import KnifeStat, NaderStat, FlagStat
-
-admin.site.register(KnifeStat, CustomModelAdmin)
-admin.site.register(NaderStat, CustomModelAdmin)
-admin.site.register(FlagStat, CustomModelAdmin)
